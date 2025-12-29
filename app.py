4c2b8bcd62ed43cd6e902a24550f29e3ce41a28d
nacattle <nacattle@uno.edu>
2025-12-28 22:02:14 -0500

    added API request to successfully retrieve broadcaster_id from channel name

--- app.py
+++ app.py
@@ -13,16 +13,32 @@ app.config['TWITCH_CLIENT_SECRET'] = os.environ.get('TWITCH_CLIENT_SECRET')
 access_token_url = 'https://id.twitch.tv/oauth2/token'
 access_token_obj = {'client_id': app.config['TWITCH_CLIENT_ID'], 'client_secret': app.config['TWITCH_CLIENT_SECRET'], 'grant_type': 'client_credentials'}
 
-access_token_resp = requests.post(access_token_url, data = access_token_obj)
-access_token = access_token_resp.json()
+access_token_res = requests.post(access_token_url, data = access_token_obj)
+access_token = access_token_res.json()
 access_token = access_token['access_token']
+
+# re-usable header for API calls
+headers = {'Authorization': f'Bearer {access_token}', 'Client-Id': app.config['TWITCH_CLIENT_ID']}
 
 # homepage
 @app.route('/', methods = ['GET', 'POST'])
 def login():
   if request.method == 'POST':
+    
+    # get channel name from template and construct obj to pass
     channel_name = request.form.get('channel_name')
-    print(f'the channel name you inputted was {channel_name}')
+    params = {'login': channel_name}
+    
+    # get broadcaster_id from channel name
+    broadcaster_id_url = 'https://api.twitch.tv/helix/users'
+    res = requests.get(broadcaster_id_url, params=params, headers=headers) 
+    data = res.json()
+    broadcaster_id = data['data'][0]['id'] # 0 points to the first json object in the list of dicts (this only returned one, but could return multiple based on the params (such as more than one login username passed))
+    
+    
+    # get clips for a channel
+    
+    
   else:
     print('user logs into twitch')
   
